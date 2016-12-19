2a55253c5146d261765dcccdea64d837ab70daac
Mohammad Afshar <ma2510@nyu.edu>
2016-12-19 02:06:45 -0500

    fixing tester code and model details

--- model/model.lua
+++ model/model.lua
@@ -14,23 +14,25 @@ local model  = nn.Sequential()
 local x = torch.Tensor(3, 70, 70)
 
 model:add(Convolution(3, 16, 4, 4))
-model:add(ReLU())
+-- model:add(ReLU())
+model:add(Tanh())
 model:add(Max(2,2,2,2))
 
 model:add(Convolution(16, 32, 4, 4))
-model:add(ReLU())
+-- model:add(ReLU())
+model:add(Tanh())
 -- model:add(Max(3,3,2,2))
 
 model:add(Convolution(32, 128, 7, 7))
 model:add(ReLU())
 model:add(Max(2,2,2,2))
-model:add(Dropout(0.2))
+-- model:add(Dropout(0.2))
 
 -- print(#model:forward(x))
 -- os.exit()
 
 
 model:add(nn.Reshape(18432))
-model:add(Linear(18432, 1000))
-model:add(Linear(1000, 321))
+model:add(Linear(18432, 4096))
+model:add(Linear(4096, 321))
 return model
--- tester.lua
+++ tester.lua
@@ -17,22 +17,29 @@ local dbg   = require 'debugger'
 local base_data_path = "/Users/mohammadafshar1/Desktop/Fall 2016/Computer_Vision/Project/facial-recognition/data/"
 
 -- local network = torch.load('./model/history/' .. config.model .. '/config' .. config.edition)
-local network = torch.load('./model/history/1/cnn_model_afshar_mohammad')
+local network = torch.load('./model/history/2/config2_final.t7')
+
+print(network)
+-- os.exit()
 
 local mlp = nn.CosineDistance()
 local threshold = 0.7
 
 local file = io.open(base_data_path .. "pairsDevTest.txt")
 local num_misclassified = 0
-local count = 1
+local count = 0
 if file then
     for line in file:lines() do
+        -- print(line)
         if (count <= 501) and (count > 1) then --right
             local name, img1, img2 = unpack(line:split("\t"))
             img1 = tonumber(img1)
             img2 = tonumber(img2)
             -- print(img1)
             -- print(img2)
+            -- if (img1 >= 100) or (img2 >= 100) then
+            --     count = count - 1
+            -- end
             if img1 < 10 then
                 img1_filepath = base_data_path .. 'lfw/' .. name .. '/' .. name .. '_000' .. img1 .. '.jpg'
             else
@@ -44,26 +51,34 @@ if file then
                 img2_filepath = base_data_path .. 'lfw/' .. name .. '/' .. name .. '_00' .. img2 .. '.jpg'
             end
             image1 = image.load(img1_filepath)
+            network:forward(image1)
+            local feat_vec1 = network:get(11).output -- second to last layer
+            print(line)
             image2 = image.load(img2_filepath)
-            local feat_vec1 = network:forward(image1).modules[-2].output -- second to last layer
-            local feat_vec2 = network:forward(image2).modules[-2].output -- second to last layer
-            print(feat_vec1)
-            print(feat_vec2)
+            network:forward(image2)
+            local feat_vec2 = network:get(11).output -- second to last layer
+            -- print('feat vec 1:')
+            -- print(feat_vec1)
+            -- print('feat vec 2:')
+            -- print(feat_vec2)
+            -- os.exit()
             prediction = mlp:forward({feat_vec1, feat_vec2})
             print(prediction)
-            if prediction < threshold then
+            if prediction[1] < threshold then
                 num_misclassified = num_misclassified + 1
             end
-        else --wrong
+        elseif (count > 501) then --wrong
             local name1, img1, name2,  img2 = unpack(line:split("\t"))
             img1 = tonumber(img1)
             img2 = tonumber(img2)
-            if img1 < 10 then
+            -- print(img1)
+            -- print(img2)
+            if (img1 < 10) then
                 img1_filepath = base_data_path .. 'lfw/' .. name1 .. '/' .. name1 .. '_000' .. img1 .. '.jpg'
             else
                 img1_filepath = base_data_path .. 'lfw/' .. name1 .. '/' .. name1 .. '_00' .. img1 .. '.jpg'
             end
-            if img2 < 10 then
+            if (img2 < 10) then
                 img2_filepath = base_data_path .. 'lfw/' .. name2 .. '/' .. name2 .. '_000' .. img2 .. '.jpg'
             else
                 img2_filepath = base_data_path .. 'lfw/' .. name .. '/' .. name2 .. '_00' .. img2 .. '.jpg'
@@ -72,14 +87,16 @@ if file then
             image2 = image.load(img2_filepath)
             local feat_vec1 = network:forward(image1).modules[-2].output -- second to last layer
             local feat_vec2 = network:forward(image2).modules[-2].output -- second to last layer
-            print(feat_vec1)
-            print(feat_vec2)
+            -- print(feat_vec1)
+            -- print(feat_vec2)
             prediction = mlp:forward({feat_vec1, feat_vec2})
             print(prediction)
-            if prediction > threshold then
+            if prediction[1] > threshold then
                 num_misclassified = num_misclassified + 1
             end
         end
         count = count + 1
     end
 end
+
+print("Test Error is: " .. num_misclassified/count)
